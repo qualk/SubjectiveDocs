27afb546285d4d2de26e750722a82101b12d38c4
Matthew Li <matthew.li10@education.nsw.gov.au>
2024-01-31 12:13:07 +1100

    Update editUrl in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,8 +20,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'SubjectiveLabs', // Usually your GitHub org/user name.
+  projectName: 'SubjectiveDocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -44,14 +44,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/SubjectiveLabs/SubjectiveDocs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/SubjectiveLabs/SubjectiveDocs',
         },
         theme: {
           customCss: './src/css/custom.css',
